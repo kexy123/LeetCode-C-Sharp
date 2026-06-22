64531a15c2e6a373f8fe6f6a769c0acfe5880d02
kexy123 <kexy12345678901@gmail.com>
2026-06-23 00:05:08 +1200

    Add case testing.

---
+++ Solution/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution p = new();
+
+        // ENTER p Solution test cases.
+        Console.WriteLine();
+    }
+}
