6a9d2dd7b042c6104b6b6f7f42747eb4763ea302
kexy123 <kexy12345678901@gmail.com>
2026-07-03 13:29:22 +1200

    Submit Combination Sum, Combination Sum II, and First Missing Positive.

---
+++ Solution/40_CombinationSumII.cs
@@ -0,0 +1,89 @@
+﻿// RESULTS:
+//      Submitted on 03 July 2026 at 11:04
+//
+//      176 / 176 testcases passed.
+//
+//      Runtime:    7 ms
+//      Memory:     47.89 MB
+//
+// This is a recursive approach compared to
+// a stack-based approach because of time
+// limit issues.
+
+public partial class Solution
+{
+    /// <summary>
+    /// Finds all unique combinations of numbers in
+    /// <paramref name="candidates"/> whose sum is
+    /// the <paramref name="target"/>. Duplicates
+    /// of combinations do not count, although it
+    /// can use elements that appear multiple times.
+    /// </summary>
+    /// <param name="candidates">An <see cref="int"/> array that can contain duplicate elements.</param>
+    /// <param name="target">The target <see cref="int"/> to sum to.</param>
+    /// <returns>The <see cref="IList{IList{int}}"/> of unique combinations.</returns>
+    public IList<IList<int>> CombinationSum2(int[] candidates, int target)
+    {
+        // Sort the array so that duplicates
+        // are next to each other.
+        Array.Sort(candidates);
+
+        int old = 0;
+
+        IList<int> combination = [];
+        IList<IList<int>> result = [];
+        for (int i = 0; i < candidates.Length; i++)
+        {
+            int value = candidates[i];
+            if (value == old)
+            {
+                // This prevents any duplicate combinations.
+                continue;
+            }
+
+            combination.Add(value);
+            DFSCombinationSum(i, value);
+            combination.Clear();
+
+            old = value;
+        }
+
+        return result;
+
+
+        // Depth-first search combination sum
+        // method. It ignores all duplicates.
+        void DFSCombinationSum(int candidateIndex, int sum)
+        {
+            // The sum is already the target,
+            // so add it to result.
+            if (sum == target)
+            {
+                result.Add([.. combination]);
+                return;
+            }
+
+            int old = 0;
+            for (int i = candidateIndex + 1; i < candidates.Length; i++)
+            {
+                int value = candidates[i];
+                if (value == old)
+                {
+                    // This prevents any duplicate
+                    // combinations form happening.
+                    continue;
+                }
+
+                int predictedSum = sum + value;
+                if (predictedSum <= target)
+                {
+                    combination.Add(value);
+                    DFSCombinationSum(i, predictedSum);
+                    combination.RemoveAt(combination.Count - 1);
+                }
+
+                old = value;
+            }
+        }
+    }
+}
---
+++ Solution/41_FirstMissingPositive.cs
@@ -0,0 +1,58 @@
+﻿// RESULTS:
+//      Submitted on 03 July 2026 at 13:25
+//
+//      179 / 179 testcases passed.
+//
+//      Runtime:    1 ms
+//      Memory:     64.49 MB
+//
+// Special thanks to ez3r_ for contributing
+// to the thinking process, and one of the
+// solutions in the LeetCode problem.
+
+public partial class Solution
+{
+    /// <summary>
+    /// Finds the lowest <see cref="int"/> not in
+    /// the <paramref name="nums"/> array that is
+    /// greater than zero.
+    /// </summary>
+    /// <param name="nums">The <see cref="int"/> array to inspect.</param>
+    /// <returns>The lowest positive <see cref="int"/> greater than zero that is not in <paramref name="nums"/>.</returns>
+    public int FirstMissingPositive(int[] nums)
+    {
+        // Variation of Counting sort; sorts all
+        // the numbers by swapping at their values
+        // being indices. However, negative numbers
+        // and numbers out of range of the nums
+        // array are ignored.
+        for (int i = 0; i < nums.Length; i++)
+        {
+            while (nums[i] - 1 != i
+                && nums[i] > 0 && nums[i] - 1 < nums.Length
+                && nums[nums[i] - 1] != nums[i])
+            {
+                // Swap the element at i with the target
+                // index being nums[i].
+                (nums[i], nums[nums[i] - 1]) = (nums[nums[i] - 1], nums[i]);
+            }
+        }
+
+        // Check for the first occurrence where
+        // Counting sort failed, in which case
+        // the first positive number that's
+        // missing in the nums array.
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] - 1 != i)
+            {
+                return i + 1;
+            }
+        }
+
+        // The entire array contains all
+        // the numbers from 1 to nums.Length,
+        // so return nums.Length + 1.
+        return nums.Length + 1;
+    }
+}
