b89ce1e207ddacb7a242c51d0b5f7e8688196be1
kexy123 <kexy12345678901@gmail.com>
2026-07-04 23:31:59 +1200

    Submit Jump Game II, Permutations, and Permutations II.

---
+++ Solution/45_JumpGameII.cs
@@ -0,0 +1,82 @@
+﻿// RESULTS:
+//      Submitted on 04 July 2026 at 20:00
+//
+//      111 / 111 testcases passed.
+//
+//      Runtime:    0 ms
+//      Memory:     45.64 MB
+//
+// First thought was a backtracking algorithm
+// similar to 44_WildcardMatching, until I
+// discovered an invariant of the array that
+// made the algorithm a simple linear pass,
+// that being the biggest range of a number
+// at a given point is related to the shortest
+// path to take.
+
+namespace Solution;
+
+public partial class Solution
+{
+    /// <summary>
+    /// Calculates the number of jumps of the
+    /// shortest path between the start and
+    /// end of the <paramref name="nums"/> array
+    /// given that the elements in the array
+    /// determine how far it can jump forward,
+    /// given that it is guaranteed for the
+    /// array to be traversable to the end.
+    /// </summary>
+    /// <param name="nums">The <see cref="int"/> array.</param>
+    /// <returns>The number of jumps to get to the end.</returns>
+    public int Jump(int[] nums)
+    {
+        int endTarget = nums.Length - 1;
+        if (nums[0] >= endTarget)
+        {
+            // The very first number already
+            // reaches the end of the array,
+            // so return 1 jump, but if the
+            // nums array only has one
+            // element, then return 0 jumps.
+            return 0 < endTarget ? 1 : 0;
+        }
+
+        int count = 1;
+        int i = 0;
+        while (i < endTarget)
+        {
+            // Jumps to the number whose
+            // range's endpoint is the
+            // furthest from all the
+            // numbers of the current
+            // range that i can go to.
+            int furthestEndpoint = 0;
+            int furthestIndex = i;
+            for (int j = i + nums[i]; j > i; j--)
+            {
+                int endpoint = j + nums[j];
+                if (endpoint > furthestEndpoint)
+                {
+                    if (endpoint >= endTarget)
+                    {
+                        // Account for the predicted
+                        // jump that can reach the
+                        // end of the nums array.
+                        return count + 1;
+                    }
+
+                    // Update furthest endpoint.
+                    furthestEndpoint = endpoint;
+                    furthestIndex = j;
+                }
+            }
+
+            i = furthestIndex;
+            count++;
+        }
+
+        // This code is unreachable.
+        return 0;
+    }
+}
---
+++ Solution/47_PermutationsII.cs
@@ -0,0 +1,141 @@
+﻿// RESULTS:
+//      Submitted on 04 July 2026 at 20:57
+//
+//      33 / 33 testcases passed.
+//
+//      Runtime:    341 ms
+//      Memory:     51.51 MB
+//
+// TODO: A backtracking recursive algorithm
+// was the fastest solution, and the intended
+// topic this question hinted on was backtracking,
+// so rewrite this algorithm when appropriate.
+
+namespace Solution;
+
+public partial class Solution
+{
+    /// <summary>
+    /// Returns all unique permutations after
+    /// <paramref name="nums"/> before going
+    /// back to itself.
+    /// </summary>
+    /// <param name="nums">The <see cref="int"/> array that can contain duplicates to permute.</param>
+    /// <returns>The array of unique permutations.</returns>
+    public IList<IList<int>> PermuteUnique(int[] nums)
+    {
+        IList<IList<int>> results = [[.. nums]];
+
+        // Ditto to 46_Permutations.
+        int numPermutations = 1;
+        for (int i = 2; i <= nums.Length; i++)
+        {
+            numPermutations *= i;
+        }
+
+        // Create the base permutation array
+        // which is the first lexicographical
+        // array in the permutation sequence,
+        // that being a sorted-ascending array.
+        int[] basePermutation = new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            basePermutation[i] = i;
+        }
+
+        // Check uniqueness if the conversion to
+        // HashSet<int> removed duplicates in the
+        // nums array. If unique, simply add all
+        // permutations.
+        bool isUnique = nums.ToHashSet().Count == nums.Length;
+
+        for (int i = 1; i < numPermutations; i++)
+        {
+            NextPermutation();
+            TryAdd();
+        }
+
+        return results;
+
+
+        // Borrowed method from 31_NextPermutation,
+        // but uses basePermutation as a mask to
+        // swap nums array.
+        void NextPermutation()
+        {
+            int k = -1, l = -1;
+            for (int i = 0; i < basePermutation.Length; i++)
+            {
+                if (i < basePermutation.Length - 1 && basePermutation[i] < basePermutation[i + 1] && i > k)
+                {
+                    k = i;
+                }
+                else if (i > l && k >= 0 && basePermutation[i] > basePermutation[k])
+                {
+                    l = i;
+                }
+            }
+
+            if (k == -1)
+            {
+                Reverse(0);
+                return;
+            }
+
+            Swap(k, l);
+            Reverse(k + 1);
+        }
+
+        void Reverse(int start)
+        {
+            int j = basePermutation.Length - 1;
+            for (int i = 0; i < (basePermutation.Length - start) / 2; i++)
+            {
+                Swap(i + start, j--);
+            }
+        }
+
+        // Swaps both elements in the basePermutation
+        // and the nums array.
+        void Swap(int i, int j)
+        {
+            (nums[i], nums[j]) = (nums[j], nums[i]);
+            (basePermutation[i], basePermutation[j]) = (basePermutation[j], basePermutation[i]);
+        }
+
+        // Attempts to add the current permutation
+        // of the nums array to the results array
+        // which must satisfy uniqueness.
+        void TryAdd()
+        {
+            if (!isUnique)
+            {
+                for (int i = 0; i < results.Count; i++)
+                {
+                    // This implementation is quicker than
+                    // nums.SequenceEquals(results[i]);
+                    // and avoids the check for length equality.
+
+                    // Check for value equality in
+                    // each element of both arrays.
+                    IList<int> permutation = results[i];
+                    for (int j = 0; j < nums.Length; j++)
+                    {
+                        if (nums[j] != permutation[j])
+                        {
+                            goto NotEqual;
+                        }
+                    }
+                    return;
+                    // Avoid the return statement.
+                NotEqual:;
+                }
+            }
+
+            // If none of the arrays in results
+            // contains the current permutation
+            // of nums, add to the result.
+            results.Add([.. nums]);
+        }
+    }
+}
