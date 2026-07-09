eaaefd7ee9028a4817a77032a7e316f453ca3e47
kexy123 <kexy12345678901@gmail.com>
2026-07-09 23:27:08 +1200

    Submit Word Search, Remove Duplicates From Sorted Array II, Search In Rotated Sorted Array II, Remove Duplicates From Sorted List II, and Remove Duplicates From Sorted List.

---
+++ Solution/80_RemoveDuplicatesFromSortedArrayII.cs
@@ -0,0 +1,72 @@
+﻿// RESULTS:
+//      Submitted on 09 July 2026 at 21:59
+//
+//      170 / 170 testcases passed.
+//
+//      Runtime:    143 ms
+//      Memory:     52.96 MB
+//
+// TODO: After personal case testing, this O(n)
+// solution should only take a few dozen microseconds
+// at best, however LeetCode says that it is over
+// 100 ms, with the fact that other solutions in C#
+// have spread-out runtimes between the 100-180 ms
+// range as well. Resubmit if they have fixed this
+// issue.
+
+namespace Solution.RemoveDuplicatesFromSortedArrayII_80;
+
+public partial class Solution
+{
+    /// <summary>
+    /// Removes all duplicates in <paramref name="nums"/>
+    /// in-place such that each element will only appear
+    /// at most twice.
+    /// </summary>
+    /// <param name="nums">The <see cref="int"/> array to remove duplicates from.</param>
+    /// <returns>The new length of the <paramref name="nums"/> array.</returns>
+    public int RemoveDuplicates(int[] nums)
+    {
+        // Track duplicates and the value
+        // of the previous element in nums.
+        bool hasDuplicate = false;
+        int old = nums[0];
+
+        int left = 1;
+        for (int right = 1; right < nums.Length; right++)
+        {
+            int rightNum = nums[right];
+            if (rightNum == old)
+            {
+                if (hasDuplicate)
+                {
+                    // This will only happen if
+                    // the element appeared more
+                    // than twice, so ignore this
+                    // element and only move the
+                    // right pointer.
+                    continue;
+                }
+
+                // This will happen if the first
+                // duplicate was found.
+                hasDuplicate = true;
+            }
+            else
+            {
+                // Reset hasDuplicate and old.
+                old = rightNum;
+                hasDuplicate = false;
+            }
+
+            // Move the rightNum to the
+            // index of the left.
+            nums[left] = rightNum;
+            left++;
+        }
+
+        // The new length of the nums
+        // array is the left pointer.
+        return left;
+    }
+}
---
+++ Solution/81_SearchInRotatedSortedArrayII.cs
@@ -0,0 +1,66 @@
+﻿// RESULTS:
+//      Submitted on 09 July 2026 at 22:42
+//
+//      285 / 285 testcases passed.
+//
+//      Runtime:    0 ms
+//      Memory:     46.10 MB
+//
+// Borrowed from 33_SearchInRotatedArray but,
+// at worst, this algorithm is O(n) because
+// of the potential ambiguity between the left,
+// the right, and the middle values and which
+// side to go for, so it simply moves the left
+// and right pointers closer together.
+
+namespace Solution.SearchInRotatedSortedArrayII_81;
+
+public partial class Solution
+{
+    /// <summary>
+    /// Checks if the <paramref name="target"/> exists
+    /// in the sorted <see cref="int"/> array
+    /// <paramref name="nums"/> where the array
+    /// is/is not right-rotated by an unknown amount
+    /// and can contain duplicates.
+    /// </summary>
+    /// <param name="nums">The <see cref="int"/> array to search through.</param>
+    /// <param name="target">The target <see cref="int"/> to find.</param>
+    /// <returns><see langword="true"/> if it does exist in <paramref name="nums"/>, otherwise <see langword="false"/>.</returns>
+    public bool Search(int[] nums, int target)
+    {
+        int left = 0, right = nums.Length - 1;
+        while (left < right)
+        {
+            int mid = left + (right - left >> 1);
+
+            int difference = target - nums[mid];
+            if (difference == 0)
+            {
+                return true;
+            }
+            // This tests if both the left, the
+            // right, and the middle pointers
+            // are all the same. In this case,
+            // it is ambiguous to which side to
+            // go to, so we have to move the left
+            // and right pointers closer together.
+            else if (nums[left] == nums[mid] && nums[mid] == nums[right])
+            {
+                left++;
+                right--;
+            }
+            else if ((nums[left] <= nums[mid] && (target < nums[left] || difference > 0))
+                || (nums[mid] <= nums[right] && difference > 0 && target <= nums[right]))
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return nums[left] == target;
+    }
+}
