f50d2fde5b14338c6eb0ef1a85543ba9ba55f96b
kexy123 <kexy12345678901@gmail.com>
2026-07-12 21:37:09 +1200

    Extract TreeNode from 94_BinaryTreeInorderReversal to LeetCodeImplementations/TreeNode.cs and refactor.

--- Solution/94_BinaryTreeInorderReversal.cs
+++ Solution/94_BinaryTreeInorderReversal.cs
@@ -6,32 +6,9 @@
 //      Runtime:    0 ms
 //      Memory:     46.45 MB
 
+using Solution.LeetCodeImplementations;
+
 namespace Solution;
-
-/// <summary>
-/// Simplified definition of <see cref="TreeNode"/>
-/// in 94_BinaryTreeInorderReversal and other questions.
-/// A binary tree node that points to two other
-/// <see cref="TreeNode"/> from its <see cref="left"/>
-/// and <see cref="right"/> and stores a value.
-/// </summary>
-public class TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
-{
-    /// <summary>
-    /// The <see cref="int"/> value of <see cref="this"/>.
-    /// </summary>
-    public int val = val;
-
-    /// <summary>
-    /// The left part of the <see cref="TreeNode"/>.
-    /// </summary>
-    public TreeNode left = left!;
-
-    /// <summary>
-    /// The right part of the <see cref="TreeNode"/>.
-    /// </summary>
-    public TreeNode right = right!;
-}
 
 public partial class Solution
 {
---
+++ Solution/LeetCodeImplementations/TreeNode.cs
@@ -0,0 +1,26 @@
+﻿namespace Solution.LeetCodeImplementations;
+
+/// <summary>
+/// Simplified definition of <see cref="TreeNode"/>
+/// in 94_BinaryTreeInorderReversal and other questions.
+/// A binary tree node that points to two other
+/// <see cref="TreeNode"/> from its <see cref="left"/>
+/// and <see cref="right"/> and stores a value.
+/// </summary>
+public class TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+{
+    /// <summary>
+    /// The <see cref="int"/> value of <see cref="this"/>.
+    /// </summary>
+    public int val = val;
+
+    /// <summary>
+    /// The left part of the <see cref="TreeNode"/>.
+    /// </summary>
+    public TreeNode left = left!;
+
+    /// <summary>
+    /// The right part of the <see cref="TreeNode"/>.
+    /// </summary>
+    public TreeNode right = right!;
+}
