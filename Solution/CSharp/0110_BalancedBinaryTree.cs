e98653a422cc11f166e339ccceddc02e89635198
kexy123 <kexy12345678901@gmail.com>
2026-07-13 20:03:43 +1200

    Submit Balanced Binary Tree and Minimum Depth of Binary Tree.

---
+++ Solution/CSharp/0110_BalancedBinaryTree.cs
@@ -0,0 +1,53 @@
+﻿// RESULTS:
+//      Submitted on 13 July 2026 at 19:45
+//
+//      228 / 228 testcases passed.
+//
+//      Runtime:    0 ms
+//      Memory:     47.12 MB
+
+using Solution.LeetCodeImplementations;
+
+namespace Solution.CSharp;
+
+public partial class Solution
+{
+    /// <summary>
+    /// Checks if the given binary <see cref="TreeNode"/>
+    /// is height-balanced, where the depth of the subtrees
+    /// in every <see cref="TreeNode"/> differ by at most
+    /// one.
+    /// </summary>
+    /// <param name="root">The starting <see cref="TreeNode"/>.</param>
+    /// <returns><see langword="true"/> if the binary <see cref="TreeNode"/> is height-balanced; otherwise <see langword="false"/>.</returns>
+    public bool IsBalanced(TreeNode? root)
+    {
+        // Check if the depth is not -1.
+        return CheckAndGetDepth(root, 0) >= 0;
+
+
+        // A recursive function that returns -1 if the
+        // difference in depth of the two subtrees of
+        // the node is greater than one or one of the
+        // subtrees themselves returned -1; otherwise
+        // returns the depth of the deeper subtree. 
+        static int CheckAndGetDepth(TreeNode? node, int depth)
+        {
+            if (node is null)
+            {
+                return depth;
+            }
+
+            depth++;
+            int leftDepth = CheckAndGetDepth(node.left, depth);
+            int rightDepth = CheckAndGetDepth(node.right, depth);
+
+            if (leftDepth == -1 || rightDepth == -1 || Math.Abs(leftDepth - rightDepth) > 1)
+            {
+                // The binary TreeNode is invalid; return -1.
+                return -1;
+            }
+            return Math.Max(leftDepth, rightDepth);
+        }
+    }
+}
---
+++ Solution/CSharp/0111_MinimumDepthOfBinaryTree.cs
@@ -0,0 +1,64 @@
+﻿// RESULTS:
+//      Submitted on 13 July 2026 at 19:58
+//
+//      53 / 53 testcases passed.
+//
+//      Runtime:    3 ms
+//      Memory:     85.75 MB
+//
+// Directly copied from 0104_MaximumDepthOfBinaryTree, but
+// moved where the minDepth variable changes and prune
+// unnecessary traversals.
+
+using Solution.LeetCodeImplementations;
+
+namespace Solution.CSharp;
+
+public partial class Solution
+{
+    /// <summary>
+    /// Finds the shortest level in the binary <see cref="TreeNode"/>
+    /// that contains a leaf <see cref="TreeNode"/>.
+    /// </summary>
+    /// <param name="root">The starting <see cref="TreeNode"/>.</param>
+    /// <returns>The shortest level in the binary tree that contains a leaf <see cref="TreeNode"/>.</returns>
+    public int MinDepth(TreeNode? root)
+    {
+        if (root is null)
+        {
+            // Avoid incorrect depth result of int.MaxValue.
+            return 0;
+        }
+
+        int minDepth = int.MaxValue;
+
+        Traverse(root, 0);
+
+        return minDepth;
+
+
+        void Traverse(TreeNode? node, int depth)
+        {
+            if (node is null || depth >= minDepth)
+            {
+                // We only need to traverse in levels
+                // lower than the minimum depth.
+                return;
+            }
+
+            depth++;
+            if (node.left is null && node.right is null)
+            {
+                // There are no subtrees at this level,
+                // so update the minimum depth as its
+                // parent is a leaf node.
+                minDepth = Math.Min(minDepth, depth);
+            }
+            else
+            {
+                Traverse(node.left, depth);
+                Traverse(node.right, depth);
+            }
+        }
+    }
+}
