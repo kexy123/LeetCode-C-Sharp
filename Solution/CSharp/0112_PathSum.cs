d262c79f460d8a3efd7865bd14793c240b3e1b15
kexy123 <kexy12345678901@gmail.com>
2026-07-14 00:03:00 +1200

    Submit Path Sum and Path Sum II.

---
+++ Solution/CSharp/0112_PathSum.cs
@@ -0,0 +1,63 @@
+﻿// RESULTS:
+//      Submitted on 13 July 2026 at 21:51
+//
+//      118 / 118 testcases passed.
+//
+//      Runtime:    0 ms
+//      Memory:     46.56 MB
+
+using Solution.LeetCodeImplementations;
+
+namespace Solution.CSharp;
+
+public partial class Solution
+{
+    /// <summary>
+    /// Checks if the given binary <see cref="TreeNode"/>
+    /// contains a path from the <paramref name="root"/>
+    /// to a leaf <see cref="TreeNode"/> where the sum of
+    /// all the <see cref="TreeNode"/> between them is
+    /// equal to the <paramref name="targetSum"/>.
+    /// </summary>
+    /// <param name="root">The starting <see cref="TreeNode"/>.</param>
+    /// <param name="targetSum">The sum to find.</param>
+    /// <returns><see langword="true"/> if a path exists; otherwise <see langword="false"/>.</returns>
+    public bool HasPathSum(TreeNode? root, int targetSum)
+    {
+        if (root is null)
+        {
+            // There are no leaf TreeNode, so do
+            // not return true, even if the
+            // targetSum is 0.
+            return false;
+        }
+
+        return TraverseForSum(root, targetSum);
+
+
+        // Traverses through the binary tree until
+        // it finds a leaf TreeNode whose sum from
+        // the root to that TreeNode is equal to
+        // the targetSum.
+        static bool TraverseForSum(TreeNode? node, int difference)
+        {
+            if (node is null)
+            {
+                return false;
+            }
+
+            difference -= node.val;
+            if (node.left is null && node.right is null)
+            {
+                // We reached a leaf TreeNode; check if
+                // the difference is zero.
+                return difference == 0;
+            }
+
+            // Subtract the difference and check
+            // the left and right subtrees.
+            return TraverseForSum(node.left, difference)
+                || TraverseForSum(node.right, difference);
+        }
+    }
+}
---
+++ Solution/CSharp/0113_PathSumII.cs
@@ -0,0 +1,68 @@
+﻿// RESULTS:
+//      Submitted on 13 July 2026 at 21:57
+//
+//      115 / 115 testcases passed.
+//
+//      Runtime:    2 ms
+//      Memory:     49.85 MB
+//
+// Borrowed from 0112_PathSum, but finds all possible
+// path sums.
+
+using Solution.LeetCodeImplementations;
+
+namespace Solution.CSharp;
+
+public partial class Solution
+{
+    /// <summary>
+    /// Returns all paths from the <paramref name="root"/>
+    /// of the binary <see cref="TreeNode"/> to the leaf
+    /// <see cref="TreeNode"/> whose sum is equal to the
+    /// <paramref name="targetSum"/>.
+    /// </summary>
+    /// <param name="root">The starting <see cref="TreeNode"/>.</param>
+    /// <param name="targetSum">The target sum.</param>
+    /// <returns>An <see cref="IList{T}"/> containing paths of <see cref="int"/> whose sum adds to the <paramref name="targetSum"/>.</returns>
+    public IList<IList<int>> PathSum(TreeNode? root, int targetSum)
+    {
+        IList<IList<int>> paths = [];
+
+        IList<int> currentPath = [];
+        TraverseForPaths(root, targetSum);
+
+        return paths;
+
+
+        void TraverseForPaths(TreeNode? node, int difference)
+        {
+            if (node is null)
+            {
+                return;
+            }
+
+            currentPath.Add(node.val);
+            difference -= node.val;
+            if (node.left is null && node.right is null)
+            {
+                if (difference == 0)
+                {
+                    // Add the path as its sum is the targetSum.
+                    paths.Add([.. currentPath]);
+                }
+
+                currentPath.RemoveAt(currentPath.Count - 1);
+
+                // Prevent unnecessary calls to TraverseForPaths.
+                return;
+            }
+
+            TraverseForPaths(node.left, difference);
+            TraverseForPaths(node.right, difference);
+
+            // Remove node element after traversal of
+            // its subtrees.
+            currentPath.RemoveAt(currentPath.Count - 1);
+        }
+    }
+}
