309b963f564d840b4abaaf8f73f8ce832740d086
kexy123 <kexy12345678901@gmail.com>
2026-07-13 19:36:57 +1200

    Submit Convert Sorted Array to Binary Search Tree and Convert Sorted List to Binary Search Tree.

---
+++ Solution/CSharp/0108_ConvertSortedArrayToBinarySearchTree.cs
@@ -0,0 +1,66 @@
+﻿// RESULTS:
+//      Submitted on 13 July 2026 at 19:09
+//
+//      31 / 31 testcases passed.
+//
+//      Runtime:    0 ms
+//      Memory:     46.08 MB
+
+using Solution.LeetCodeImplementations;
+
+namespace Solution.CSharp;
+
+public partial class Solution
+{
+    /// <summary>
+    /// Builds a height-balanced binary search tree
+    /// from the given sorted <see cref="int"/> array
+    /// <paramref name="nums"/> where the subtrees of
+    /// every <see cref="TreeNode"/> in the tree have
+    /// a max depth that differ by at most one.
+    /// </summary>
+    /// <param name="nums">The sorted <see cref="int"/> array.</param>
+    /// <returns>The starting <see cref="TreeNode"/>.</returns>
+    public TreeNode SortedArrayToBST(int[] nums)
+    {
+        TreeNode root = new();
+
+        Build(root, 0, nums.Length - 1);
+
+        return root;
+
+
+        // Builds the TreeNode's subtrees from the range
+        // of the nums array.
+        void Build(TreeNode node, int left, int right)
+        {
+            int mid = left + (right - left >> 1);
+            node.val = nums[mid];
+
+            if (mid == right)
+            {
+                // The mid is the midpoint between left and
+                // right, with ties in favor of the left
+                // pointer. Therefore, if mid == right, then
+                // left == right too, meaning that there are
+                // no other nodes to branch off to.
+                return;
+            }
+
+            // Branch to the right node.
+            node.right = new();
+            Build(node.right, mid + 1, right);
+
+            // If mid == left, then left and right are
+            // right next to each other, and since we
+            // already wrote the value for mid, we only
+            // need to branch the right.
+            if (mid != left)
+            {
+                // Otherwise, branch to the left node.
+                node.left = new();
+                Build(node.left, left, mid - 1);
+            }
+        }
+    }
+}
---
+++ Solution/CSharp/0109_ConvertSortedListToBinarySearchTree.cs
@@ -0,0 +1,73 @@
+﻿// RESULTS:
+//      Submitted on 13 July 2026 at 19:30
+//
+//      32 / 32 testcases passed.
+//
+//      Runtime:    1 ms
+//      Memory:     49.29 MB
+//
+// Directly copied from 0108_ConvertSortedArrayToBinarySearchTree,
+// but simply transforming the linked ListNode to an array and
+// adding an empty ListNode check to return an empty tree.
+
+using Solution.LeetCodeImplementations;
+
+namespace Solution.CSharp;
+
+public partial class Solution
+{
+    /// <summary>
+    /// Builds a height-balanced binary search tree
+    /// from the given sorted <see cref="int"/> linked
+    /// <see cref="ListNode"/> <paramref name="nums"/>
+    /// where the subtrees of every <see cref="TreeNode"/>
+    /// in the tree have a max depth that differ by at
+    /// most one.
+    /// </summary>
+    /// <param name="nums">The starting <see cref="ListNode"/>.</param>
+    /// <returns>The starting <see cref="TreeNode"/>.</returns>
+    public TreeNode? SortedListToBST(ListNode? nums)
+    {
+        if (nums is null)
+        {
+            // Avoid an ArgumentOutOfRangeException from
+            // a faulty right pointer, and simply return
+            // an empty tree.
+            return null;
+        }
+
+        List<int> numList = [];
+        while (nums is not null)
+        {
+            numList.Add(nums.val);
+            nums = nums.next;
+        }
+
+        TreeNode root = new();
+
+        Build(root, 0, numList.Count - 1);
+
+        return root;
+
+
+        void Build(TreeNode node, int left, int right)
+        {
+            int mid = left + (right - left >> 1);
+            node.val = numList[mid];
+
+            if (mid == right)
+            {
+                return;
+            }
+
+            node.right = new();
+            Build(node.right, mid + 1, right);
+
+            if (mid != left)
+            {
+                node.left = new();
+                Build(node.left, left, mid - 1);
+            }
+        }
+    }
+}
