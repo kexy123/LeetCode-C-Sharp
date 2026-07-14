63907405011bfd0c6d0065cd6823ba4337af5499
kexy123 <kexy12345678901@gmail.com>
2026-07-14 23:42:09 +1200

    Submit Pascal's Triangle and Pascal's Triangle II.

---
+++ Solution/CSharp/0118_PascalsTriangle.cs
@@ -0,0 +1,58 @@
+﻿// RESULTS:
+//      Submitted on 14 July 2026 at 23:23
+//
+//      30 / 30 testcases passed.
+//
+//      Runtime:    0 ms
+//      Memory:     40.12 MB
+
+namespace Solution.CSharp;
+
+public partial class Solution
+{
+    /// <summary>
+    /// Generates the sequence of Pascal's
+    /// triangle divided into its rows as
+    /// a triangular array.
+    /// </summary>
+    /// <param name="numRows">The number of rows to generate.</param>
+    /// <returns>The <see cref="IList{T}"/> of rows of Pascal's triangle.</returns>
+    public IList<IList<int>> Generate(int numRows)
+    {
+        IList<IList<int>> rows = [];
+
+        int[] prevRow = [1];
+        for (int i = 1; i < numRows; i++)
+        {
+            rows.Add(prevRow);
+
+            // The first and last elements of
+            // the row are always 1.
+            int[] row = new int[i + 1];
+            row[0] = 1;
+            row[^1] = 1;
+
+            for (int j = 1; j < row.Length - 1; j++)
+            {
+                // 1
+                // 1 1
+                // 1 # 1
+                // 1 # # 1
+                //
+                // The # are the numbers that are to be
+                // computed, which is the sum of its top
+                // and top-left adjacent elements in the
+                // diagram.
+                row[j] = prevRow[j - 1] + prevRow[j];
+            }
+
+            prevRow = row;
+        }
+
+        // Prevent unnecessary computation of
+        // the next row.
+        rows.Add(prevRow);
+
+        return rows;
+    }
+}
---
+++ Solution/CSharp/0119_PascalsTriangleII.cs
@@ -0,0 +1,42 @@
+﻿// RESULTS:
+//      Submitted on 14 July 2026 at 23:37
+//
+//      34 / 34 testcases passed.
+//
+//      Runtime:    0 ms
+//      Memory:     39.94 MB
+
+namespace Solution.CSharp;
+
+public partial class Solution
+{
+    /// <summary>
+    /// Gets the row of Pascal's triangle.
+    /// </summary>
+    /// <param name="rowIndex">The row of Pascal's triangle to get.</param>
+    /// <returns>The <see cref="int"/> array.</returns>
+    public IList<int> GetRow(int rowIndex)
+    {
+        // Set all elements in the row to 1.
+        int[] row = new int[rowIndex + 1];
+        for (int i = 0; i <= rowIndex; i++)
+        {
+            row[i] = 1;
+        }
+
+        for (int i = 2; i <= rowIndex; i++)
+        {
+            // Keep track of the previous element
+            // before it got its sum to compute
+            // Pascal's triangle. Note that
+            // row[0] = 1.
+            int previous = 1;
+            for (int j = 1; j < i; j++)
+            {
+                (previous, row[j]) = (row[j], row[j] + previous);
+            }
+        }
+
+        return row;
+    }
+}
