c7a681852f1cfd7918c4220c90e63b9b5c23ca60
kexy123 <kexy12345678901@gmail.com>
2026-07-18 00:03:07 +1200

    Submit Binary Tree Maximum Path Sum and Is Valid Palindrome.

---
+++ Solution/CSharp/0124_BinaryTreeMaximumPathSum.cs
@@ -0,0 +1,66 @@
+﻿// RESULTS:
+//      Submitted on 17 July 2026 at 22:20
+//
+//      96 / 96 testcases passed.
+//
+//      Runtime:    0 ms
+//      Memory:     48.47 MB
+//
+// Was nearly about to look up a solution given my old
+// convoluted method, until I found a different approach
+// to traversing the binary tree.
+
+using Solution.LeetCodeImplementations;
+
+namespace Solution.CSharp;
+
+public partial class Solution
+{
+    /// <summary>
+    /// Computes the maximum sum possible of a
+    /// non-empty path that traverses adjacent
+    /// <see cref="TreeNode"/> instances in the
+    /// binary tree only once.
+    /// </summary>
+    /// <param name="root">The starting <see cref="TreeNode"/>.</param>
+    /// <returns>The maximum sum possible.</returns>
+    public int MaxPathSum(TreeNode root)
+    {
+        int maxSum = int.MinValue;
+        Traverse(root);
+
+        return maxSum;
+
+
+        // Finds the maximum sum possible in the current
+        // TreeNode that connects to this TreeNode, and
+        // sets the maxSum if possible to the maximum
+        // parent sum or the sum of the left and right
+        // subtrees. Note that the sum of this method can
+        // never contain the sum of its left and right
+        // subtrees.
+        int Traverse(TreeNode node)
+        {
+            if (node is null)
+            {
+                return 0;
+            }
+
+            int leftSum = Traverse(node.left);
+            int rightSum = Traverse(node.right);
+
+            // The sum that can be returned is either itself,
+            // itself plus the maximum sum of the left subtree,
+            // or itself plus the maximum sum of the right
+            // subtree.
+            int maxParentSum = Math.Max(node.val, node.val + Math.Max(leftSum, rightSum));
+            
+            // And then the maxSum can contain the maxParentSum
+            // or the sum of the left and right subtrees connected
+            // by this TreeNode.
+            maxSum = Math.Max(maxSum, Math.Max(maxParentSum, leftSum + node.val + rightSum));
+
+            return maxParentSum;
+        }
+    }
+}
---
+++ Solution/CSharp/0125_IsValidPalindrome.cs
@@ -0,0 +1,78 @@
+﻿// RESULTS:
+//      Submitted on 17 July 2026 at 22:50
+//
+//      490 / 490 testcases passed.
+//
+//      Runtime:    1 ms
+//      Memory:     44.71 MB
+
+namespace Solution.CSharp;
+
+public partial class Solution
+{
+    /// <summary>
+    /// Checks if the given <see cref="string"/> <paramref name="s"/>
+    /// is a palindrome with case-insensitivity and only accounting
+    /// for alphanumeric characters (Latin characters and digits).
+    /// </summary>
+    /// <param name="s">The <see cref="string"/> to inspect.</param>
+    /// <returns><see langword="true"/> if it is a palindrome; otherwise <see langword="false"/>.</returns>
+    public bool IsPalindrome(string s)
+    {
+        int left = 0;
+        int right = s.Length - 1;
+
+        // We only need to check if the string is a
+        // palindrome by going over half of the
+        // string and simply checking the other end.
+        while (left < right)
+        {
+            char leftC = s[left], rightC = s[right];
+            if (!IsAlphanumeric(leftC))
+            {
+                left++;
+                continue;
+            }
+            else if (!IsAlphanumeric(rightC))
+            {
+                right--;
+                continue;
+            }
+
+            // Uppercase Latin characters in ASCII in
+            // binary form are prefixed with 010, while
+            // lowercase Latin characters are prefixed
+            // with 011, so the bitwise AND operation
+            // transforms all lowercase characters to
+            // uppercase characters automatically.
+            if (leftC is >= 'a' and <= 'z')
+            {
+                leftC &= (char)0b01011111;
+            }
+            if (rightC is >= 'a' and <= 'z')
+            {
+                rightC &= (char)0b01011111;
+            }
+
+            if (leftC != rightC)
+            {
+                // The characters on the left and right
+                // are not the same, so the string is
+                // not a palindrome.
+                return false;
+            }
+
+            // Move the two pointers.
+            left++;
+            right--;
+        }
+
+        return true;
+
+
+        // Checks if the given character c is alphanumeric,
+        // which means that it is either an uppercase Latin
+        // character, a lowercase Latin character, or a digit.
+        static bool IsAlphanumeric(char c) => c is >= 'A' and <= 'Z' or >= 'a' and <= 'z' or >= '0' and <= '9';
+    }
+}
