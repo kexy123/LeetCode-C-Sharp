b8a0953112415139f7f9aeb17a1b98ae82124fe1
kexy123 <kexy12345678901@gmail.com>
2026-08-15 14:51:49 +1200

    Submit Palindrome Partitioning and Palindrome Partitioning II.

---
+++ Solution/CSharp/0131_PalindromePartitioning.cs
@@ -0,0 +1,93 @@
+﻿// RESULTS:
+//      Submitted on 15 August 2026 at 12:26
+//
+//      32 / 32 testcases passed.
+//
+//      Runtime:    45 ms
+//      Memory:     84.41 MB
+//
+// Originally thought of how to check if a string is a palindrome without the endpoint,
+// until I realized that the complexity for doing such a method is roughly equivalent to
+// just checking if its a palindrome from the end of the string, so there wasn't a purpose
+// to use Manacher's algorithm:
+// https://en.wikipedia.org/wiki/Longest_palindromic_substring#Manacher's_algorithm
+
+namespace Solution.CSharp;
+
+public partial class Solution
+{
+    /// <summary>
+    /// Returns every possible partition of <paramref name="s"/> such that each partition is
+    /// a palindrome.
+    /// </summary>
+    /// <param name="s">The <see langword="string"/> to partition.</param>
+    /// <returns>Every possible partition sequence.</returns>
+    public IList<IList<string>> Partition(string s)
+    {
+        IList<IList<string>> result = [];
+        IList<string> current = [];
+
+        List<int>[] partitionIndices = new List<int>[s.Length];
+        Split(0);
+
+        return result;
+
+
+        // A depth-first search subroutine that partitions at every valid palindrome of s
+        // starting at the given index, and calls itself with the index being the end of
+        // the partitioned substring.
+        void Split(int index)
+        {
+            if (index >= s.Length)
+            {
+                // Reached the end, so add the partition sequence to the results.
+                result.Add([.. current]);
+                return;
+            }
+
+            if (partitionIndices[index] is List<int> indices)
+            {
+                // The partition indices has already been computed, so use them.
+                foreach (int cutIndex in indices)
+                {
+                    current.Add(s[index..cutIndex]);
+                    Split(cutIndex);
+                    current.RemoveAt(current.Count - 1);
+                }
+
+                return;
+            }
+
+            indices = partitionIndices[index] = [];
+
+            // Run through the string s from index, and check for palindrome validity.
+            int cutAttempt = s.Length;
+            while (cutAttempt > index)
+            {
+                // Check if s[index..cutAttempt] is a palindrome.
+                int left = index, right = cutAttempt - 1;
+                while (right > left)
+                {
+                    if (s[left] != s[right])
+                    {
+                        break;
+                    }
+                    right--;
+                    left++;
+                }
+
+                // The slice s[index..cutAttempt] is a palindrome, so partition here.
+                if (right <= left)
+                {
+                    indices.Add(cutAttempt);
+
+                    current.Add(s[index..(cutAttempt)]);
+                    Split(cutAttempt);
+                    current.RemoveAt(current.Count - 1);
+                }
+
+                cutAttempt--;
+            }
+        }
+    }
+}
---
+++ Solution/CSharp/0132_PalindromePartitioningII.cs
@@ -0,0 +1,69 @@
+﻿// RESULTS:
+//      Submitted on 15 August 2026 at 14:42
+//
+//      37 / 37 testcases passed.
+//
+//      Runtime:    811 ms
+//      Memory:     39.29 MB
+
+namespace Solution.CSharp;
+
+public partial class Solution
+{
+    /// <summary>
+    /// Returns the minimum number of cuts to partition <paramref name="s"/> such that each
+    /// partition is a palindrome.
+    /// </summary>
+    /// <param name="s">The <see langword="string"/> to partition.</param>
+    /// <returns>The minimum number of cuts to partition <paramref name="s"/>.</returns>
+    public int MinCut(string s)
+    {
+        int[] minimumCuts = new int[s.Length];
+
+        return Split(0) - 1;
+
+
+        // A depth-first search subroutine that partitions at every valid palindrome of s
+        // starting at the given index, and checks if it yields the minimum number of cuts
+        // at that point as well.
+        int Split(int index)
+        {
+            if (index >= s.Length)
+            {
+                return 0;
+            }
+
+            ref int minCuts = ref minimumCuts[index];
+            if (minCuts is > 0)
+            {
+                return minCuts;
+            }
+
+            minCuts = int.MaxValue;
+            int cutAttempt = s.Length;
+            while (cutAttempt > index)
+            {
+                int left = index, right = cutAttempt - 1;
+                while (right > left)
+                {
+                    if (s[left] != s[right])
+                    {
+                        break;
+                    }
+                    right--;
+                    left++;
+                }
+
+                if (right <= left)
+                {
+                    // Check if the current split at cutAttempt has the smallest number of cuts.
+                    minCuts = Math.Min(minCuts, Split(cutAttempt));
+                }
+
+                cutAttempt--;
+            }
+
+            return ++minCuts;
+        }
+    }
+}
