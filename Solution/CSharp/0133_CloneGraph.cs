6e375833bb95cdfaa67ef6303cbdc26bc2110d89
kexy123 <kexy12345678901@gmail.com>
2026-08-15 15:53:01 +1200

    Submit Clone Graph and implement Bidirectional Node.

---
+++ Solution/CSharp/0133_CloneGraph.cs
@@ -0,0 +1,57 @@
+﻿// RESULTS:
+//      Submitted on 15 August 2026 at 15:46
+//
+//      22 / 22 testcases passed.
+//
+//      Runtime:    138 ms
+//      Memory:     49.63 MB
+
+using Node = Solution.LeetCodeImplementations.CloneGraph_0133.Node;
+
+namespace Solution.CSharp;
+
+public partial class Solution
+{
+    /// <summary>
+    /// Deep clones a graph of <see cref="Node"/> instances.
+    /// </summary>
+    /// <param name="node">One of the <see cref="Node"/> instances in the graph.</param>
+    /// <returns>The cloned graph starting at the duplicate of <paramref name="node"/>.</returns>
+    public Node? CloneGraph(Node? node)
+    {
+        if (node is null)
+        {
+            return null;
+        }
+
+        Dictionary<Node, Node> clonedNodes = [];
+        Queue<Node> queue = [];
+        queue.Enqueue(node);
+        while (queue.TryDequeue(out Node? currentNode))
+        {
+            // Create a new clone of the currentNode.
+            clonedNodes[currentNode] = new(currentNode.val);
+
+            foreach (Node neighbor in currentNode.neighbors)
+            {
+                if (!clonedNodes.ContainsKey(neighbor))
+                {
+                    // Enqueue the next Node to clone.
+                    queue.Enqueue(neighbor);
+                }
+            }
+        }
+
+        foreach (KeyValuePair<Node, Node> entry in clonedNodes)
+        {
+            // Connect the neighbors of the clone to other clones
+            // that correspond to the original.
+            foreach (Node neighbor in entry.Key.neighbors)
+            {
+                entry.Value.neighbors.Add(clonedNodes[neighbor]);
+            }
+        }
+
+        return clonedNodes[node];
+    }
+}
---
+++ Solution/LeetCodeImplementations/BidirectionalNode.cs
@@ -0,0 +1,46 @@
+﻿namespace Solution.LeetCodeImplementations.CloneGraph_0133;
+
+/// <summary>
+/// Simplified definition of <see cref="Node"/> in
+/// 0133_CloneGraph: a bidirectional graph node that
+/// stores a value and connects to an arbitrary number
+/// of <see cref="Node"/> that are considered its
+/// <see cref="neighbors"/>. Two <see cref="Node"/>
+/// connected by an edge must contain each other in its
+/// <see cref="neighbors"/> field.
+/// </summary>
+public class Node
+{
+    /// <summary>
+    /// The value of the <see cref="Node"/> instance.
+    /// </summary>
+    public int val;
+
+    /// <summary>
+    /// The neighboring <see cref="Node"/> instances that it's pointing to.
+    /// </summary>
+    public IList<Node> neighbors;
+
+
+    /// <summary>
+    /// Creates a lone <see cref="Node"/> instance.
+    /// </summary>
+    /// <param name="_val">The value of the <see cref="Node"/>. Defaults to 0.</param>
+    public Node(int _val = 0)
+    {
+        val = _val;
+        neighbors = [];
+    }
+
+    /// <summary>
+    /// Creates a <see cref="Node"/> instance that connects to neighboring
+    /// <see cref="Node"/> instances.
+    /// </summary>
+    /// <param name="_val">The value of the <see cref="Node"/>.</param>
+    /// <param name="_neighbors">An <see cref="IList{T}"/> of its neighboring nodes.</param>
+    public Node(int _val, IList<Node> _neighbors)
+    {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
