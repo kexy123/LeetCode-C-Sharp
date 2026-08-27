9f34e8a8d76e8e30847646b6a419f4a40523ceb2
kexy123 <kexy12345678901@gmail.com>
2026-08-27 23:52:02 +1200

    Submit LRU Cache and Insertion Sort List.

---
+++ Solution/CSharp/0146_LRUCache.cs
@@ -0,0 +1,119 @@
+﻿// RESULTS:
+//      Submitted on 27 August 2026 at 19:41
+//
+//      23 / 23 testcases passed.
+//
+//      Runtime:    8 ms
+//      Memory:     75.36 MB
+
+namespace Solution.CSharp;
+
+public partial class Solution;
+
+/// <summary>
+/// A cache class with a given <see cref="Capacity"/> that evicts the least-recently used (LRU)
+/// cache entries in terms of access and assignment.
+/// </summary>
+public class LRUCache
+{
+    /// <summary>
+    /// A cache entry that stores the <see cref="Value"/> and the assigned <see cref="RecencyNode"/>.
+    /// </summary>
+    private class CacheEntry()
+    {
+        /// <summary>
+        /// The stored <see langword="int"/> value.
+        /// </summary>
+        public required int Value;
+
+        /// <summary>
+        /// The <see cref="LinkedListNode{T}"/> that this <see cref="CacheEntry"/> instance is
+        /// assigned to.
+        /// </summary>
+        public required LinkedListNode<int> RecencyNode;
+    }
+
+    /// <summary>
+    /// A doubly-linked list of <see langword="int"/> keys to the <see cref="_entries"/> dictionary.
+    /// </summary>
+    private readonly LinkedList<int> _recency = [];
+
+    /// <summary>
+    /// A dictionary that stores an <see langword="int"/> key to a <see cref="CacheEntry"/> instance.
+    /// </summary>
+    private readonly Dictionary<int, CacheEntry> _entries = [];
+
+    /// <summary>
+    /// The maximum number of cache entries that this <see cref="LRUCache"/> can hold before having to
+    /// evict least-recently used cache entries.
+    /// </summary>
+    public int Capacity { get; private set; }
+
+
+    /// <summary>
+    /// Creates an <see cref="LRUCache"/> instance with the given <paramref name="capacity"/>.
+    /// </summary>
+    /// <param name="capacity">The maximum number of cache entries.</param>
+    public LRUCache(int capacity)
+    {
+        Capacity = capacity;
+    }
+
+    /// <summary>
+    /// Retrieves the value at the given <paramref name="key"/> in O(1) average time complexity.
+    /// Returns -1 if no value was found in any cache entries.
+    /// </summary>
+    /// <param name="key">The <see langword="int"/> key to access.</param>
+    /// <returns>The value at that <paramref name="key"/>; otherwise -1.</returns>
+    public int Get(int key)
+    {
+        if (_entries.TryGetValue(key, out CacheEntry? cacheEntry))
+        {
+            // This cacheEntry was accessed, so move it to the most-recent position in the LinkedList.
+            _recency.Remove(cacheEntry.RecencyNode);
+            _recency.AddLast(cacheEntry.RecencyNode);
+
+            return cacheEntry.Value;
+        }
+
+        return -1;
+    }
+
+    /// <summary>
+    /// Adds or reassigns a given <paramref name="key"/> to a <paramref name="value"/>, and evicts the
+    /// least-recently used cache entry if this <see cref="LRUCache"/> instance exceeds
+    /// the <see cref="Capacity"/> in O(1) average time complexity.
+    /// </summary>
+    /// <param name="key">The <see langword="int"/> key to assign or add.</param>
+    /// <param name="value">The <see langword="int"/> value to assign to.</param>
+    public void Put(int key, int value)
+    {
+        if (_entries.TryGetValue(key, out CacheEntry? cacheEntry))
+        {
+            // This cacheEntry was recently accessed.
+            _recency.Remove(cacheEntry.RecencyNode);
+            _recency.AddLast(cacheEntry.RecencyNode);
+
+            cacheEntry.Value = value;
+
+            return;
+        }
+
+        if (_entries.Count >= Capacity)
+        {
+            // The number of entries will overflow, so remove the very first LinkedListNode from the
+            // LinkedList. The LinkedList is managed in such a way that it is ordered from
+            // least-recently used to most-recently used from First to Last, so we can simply remove
+            // the First element and its CacheEntry, which are O(1) operations.
+            _entries.Remove(_recency.First!.Value);
+            _recency.RemoveFirst();
+        }
+
+        // Add the entry as the most-recent entry.
+        _entries[key] = new CacheEntry()
+        {
+            Value = value,
+            RecencyNode = _recency.AddLast(key)
+        };
+    }
+}
---
+++ Solution/CSharp/0147_InsertionSortList.cs
@@ -0,0 +1,64 @@
+﻿// RESULTS:
+//      Submitted on 27 August 2026 at 21:06
+//
+//      19 / 19 testcases passed.
+//
+//      Runtime:    3 ms
+//      Memory:     46.73 MB
+
+namespace Solution.CSharp;
+
+public partial class Solution
+{
+    /// <summary>
+    /// Sorts the linked list using <see href="https://en.wikipedia.org/wiki/Insertion_sort">Insertion
+    /// sort</see> and returns the first <see cref="ListNode"/> in ascending order.
+    /// </summary>
+    /// <param name="head">The starting <see cref="ListNode"/>.</param>
+    /// <returns>The first <see cref="ListNode"/> of the sorted linked list.</returns>
+    public ListNode InsertionSortList(ListNode head)
+    {
+        ListNode start = new(int.MinValue, head);
+
+        // Go through the linked list.
+        ListNode? current = head, previous = start;
+        while (current is not null)
+        {
+            ListNode? oldRight = current.next;
+            if (current.val < previous.val)
+            {
+                // The linked list is not in ascending order at previous -> current, so perform
+                // an insertion.
+
+                ListNode newLeft = start;
+                while (newLeft.next.val < current.val)
+                {
+                    // Find a point in the linked list that is newLeft -> newRight such that
+                    // newLeft <= current <= newRight. In which case we only need current <= newRight.
+                    newLeft = newLeft.next;
+                }
+
+                ListNode newRight = newLeft.next;
+
+                // We need to dissolve previous -> current -> oldRight into previous -> oldRight.
+                previous.next = oldRight;
+
+                // Then insert current in-between newLeft -> newRight to make
+                // newLeft -> current -> newRight.
+                newLeft.next = current;
+                current.next = newRight;
+
+                // Note that we do not change the previous as current will be oldRight whose previous
+                // is the same.
+            }
+            else
+            {
+                previous = current;
+            }
+
+            current = oldRight;
+        }
+
+        return start.next;
+    }
+}
