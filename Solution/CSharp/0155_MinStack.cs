5b97c02d1e0d7b6536808ae8f1b4a27159e57e71
kexy123 <kexy12345678901@gmail.com>
2026-09-01 10:43:35 +1200

    Submit MinStack and Intersection of Two Linked Lists.

---
+++ Solution/CSharp/0155_MinStack.cs
@@ -0,0 +1,69 @@
+﻿// RESULTS:
+//      Submitted on 1 September 2026 at 10:27
+//
+//      45 / 45 testcases passed.
+//
+//      Runtime:    13 ms
+//      Memory:     99.82 MB
+
+namespace Solution.CSharp;
+
+public partial class Solution;
+
+/// <summary>
+/// A <see cref="Stack{T}"/> of <see langword="int"/> that tracks the minimum value.
+/// </summary>
+public class MinStack()
+{
+    /// <summary>
+    /// The minimum value in the <see cref="_stack"/>.
+    /// </summary>
+    private int _minValue = int.MaxValue;
+
+    /// <summary>
+    /// The <see cref="Stack{T}"/> of <see langword="int"/> values that stores the lowest
+    /// <see langword="int"/> for each entry.
+    /// </summary>
+    private readonly Stack<(int value, int min)> _stack = [];
+
+
+    /// <summary>
+    /// Pushes a <paramref name="value"/> to the stack.
+    /// </summary>
+    /// <param name="value">The <see langword="int"/> to push.</param>
+    public void Push(int value)
+    {
+        // Associate the entry with the current _minValue in the stack.
+        _stack.Push((value, _minValue));
+
+        // Change _minValue if the current value is lower.
+        _minValue = Math.Min(_minValue, value);
+    }
+
+    /// <summary>
+    /// Pops the first element in the stack.
+    /// </summary>
+    public void Pop()
+    {
+        (int _, int min) = _stack.Pop();
+        _minValue = min;
+    }
+
+    /// <summary>
+    /// Peeks at the first element in the stack.
+    /// </summary>
+    /// <returns>The value of the first element.</returns>
+    public int Top()
+    {
+        return _stack.Peek().value;
+    }
+
+    /// <summary>
+    /// Gets the lowest value in the stack.
+    /// </summary>
+    /// <returns>The lowest <see langword="int"/> value in the stack.</returns>
+    public int GetMin()
+    {
+        return _minValue;
+    }
+}
---
+++ Solution/CSharp/0160_IntersectionOfTwoLinkedLists.cs
@@ -0,0 +1,67 @@
+﻿// RESULTS:
+//      Submitted on 01 September 2026 at 10:36
+//
+//      41 / 41 testcases passed.
+//
+//      Runtime:    162 ms
+//      Memory:     70.14 MB
+
+namespace Solution.CSharp;
+
+public partial class Solution
+{
+    /// <summary>
+    /// Finds the intersecting node of two linked lists <paramref name="headA"/> and
+    /// <paramref name="headB"/>, otherwise return <see langword="null"/>.
+    /// </summary>
+    /// <param name="headA">A starting <see cref="ListNode"/>.</param>
+    /// <param name="headB">A starting <see cref="ListNode"/>.</param>
+    /// <returns>The intersection of <paramref name="headA"/> and <paramref name="headB"/>, otherwise null.</returns>
+    public ListNode? GetIntersectionNode(ListNode headA, ListNode headB)
+    {
+        ListNode endA = headA;
+        while (endA.next is not null)
+        {
+            endA = endA.next;
+        }
+
+        // Link the linked list of headA back to itself to create a cycle.
+        endA.next = headA;
+
+        // Directly borrowed from 0142_LinkedListCycleII. Since the linked list at headA is a cycle,
+        // if headB intersects headA, then headB must also be in a cycle. For a linked list such as:
+        //  1 -> 2 -> 3 -> 4 ->
+        //             \       \
+        //              <- 6 <- 5
+        //
+        // where headA is a cycle at 3 and headB starts at 1, Floyd's algorithm can find the
+        // intersection of the slow and fast nodes, which also finds the intersection of headA
+        // and headB.
+        ListNode slow = headB, fast = headB;
+        while (fast.next is ListNode next && next.next is ListNode nextNext)
+        {
+            slow = slow.next;
+            fast = nextNext;
+
+            if (slow == fast)
+            {
+                goto CycleDetected;
+            }
+        }
+
+        // Note that the linked list at endA must retain its structure.
+        endA.next = null!;
+        return null;
+
+    CycleDetected:
+        fast = headB;
+        while (fast != slow)
+        {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        endA.next = null!;
+        return slow;
+    }
+}
